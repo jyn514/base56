0117fca3442e670d649d84d0d9b7dc93570af61b
Joshua Nelson <jyn514@gmail.com>
2018-02-22 19:09:32 -0500

    add more test cases

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 from base56 import encode, decode
+from string import printable
 
-def test_end_to_end():
-    for data in [ 'hi', 'there', "let's try Unicode", 'μ', 'Α α',
-            'Β β', 'Γ γ', 'Ξ ξ', '[ð]', 'εγγεγραμμένος', '𐀆', '𐀅']:
-            assert decode(encode(data)) == data
 
+def test_printable_letters():
+    for char in printable:
+        assert decode(encode(char)) == char
+
+
+def test_unicode():
+    for data in ['hi', 'there', "let's try Unicode", 'μ', 'Α α',
+                 'Β β', 'Γ γ', 'Ξ ξ', '[ð]', 'εγγεγραμμένος', '𐀆', '𐀅']:
+        assert decode(encode(data)) == data
+
+
+def test_binary():
+    for data in b'\x80\x81\x99\x10\x54':
+        assert decode(encode(bytes([data]))) == bytes([data])
